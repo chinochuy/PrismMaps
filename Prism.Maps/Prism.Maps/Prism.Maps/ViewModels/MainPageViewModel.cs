f492f183ab7c97536a5ec26cbf3c5f0377fc989a
chinochuy <padillaarizpe_@hotmail.com>
2017-04-28 21:24:09 -0500

    Proyecto Prism con Google Services (Error)
    Proyecto con error

---
+++ Prism.Maps/Prism.Maps/Prism.Maps/ViewModels/MainPageViewModel.cs
@@ -0,0 +1,34 @@
+﻿using Prism.Commands;
+using Prism.Mvvm;
+using Prism.Navigation;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+
+namespace Prism.Maps.ViewModels
+{
+    public class MainPageViewModel : BindableBase, INavigationAware
+    {
+
+
+        public MainPageViewModel()
+        {
+
+        }
+
+        public void OnNavigatedFrom(NavigationParameters parameters)
+        {
+
+        }
+
+        public void OnNavigatingTo(NavigationParameters parameters)
+        {
+
+        }
+
+        public void OnNavigatedTo(NavigationParameters parameters)
+        {
+
+        }
+    }
+}
